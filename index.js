97f49e28af8c388b7c4781ae9e45192b5279a4b5
matt8poi <matt8poi@gmail.com>
2020-05-20 17:25:55 -0600

    deploy

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
 
-app.use(express.json())
+const cors = require('cors')
+
 app.use(cors())
+app.use(express.json())
 
 let notes = [
     {
@@ -26,10 +26,6 @@ let notes = [
       important: true
     }
   ]
-
-  app.get('/', (req,res) => {
-      res.send('<h1>Hello World!!!</h1>')
-  })
 
   app.get('/api/notes', (req, res) => {
       res.json(notes)
@@ -82,7 +78,7 @@ let notes = [
       response.status(204).end()
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
   })
