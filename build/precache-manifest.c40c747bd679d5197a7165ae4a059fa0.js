3147c3561ba838e272ebf0c26fef957e089f2b21
matt8poi <matt8poi@gmail.com>
2020-05-20 18:04:39 -0600

    added

---
+++ build/precache-manifest.c40c747bd679d5197a7165ae4a059fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3274bcd0c4d6a54eebbc4263b12ff474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4c47c7ba4461d3cf0bb",
+    "url": "/static/css/main.f993a7af.chunk.css"
+  },
+  {
+    "revision": "4a2faa2cc784cc83e4e1",
+    "url": "/static/js/2.733e7ffb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.733e7ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c47c7ba4461d3cf0bb",
+    "url": "/static/js/main.141d591f.chunk.js"
+  },
+  {
+    "revision": "f7657097e0a95b329c1c",
+    "url": "/static/js/runtime-main.614a7c63.js"
+  }
+]);
